6dd74452016c96dd36917934fadd77b4ca7f2a68
sharagulov <justcausegame@yandex.ru>
2025-06-30 17:54:46 +0300

    fix: backend build fails

--- backend/src/controllers/markdown.controller.ts
+++ backend/src/controllers/markdown.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import fs from "fs";
-import { DATA_PATH } from "../config";
+import { DATA_DIR } from "../config";
 
 export function getMarkdown(req: Request, res: Response) {
-  const text = fs.existsSync(DATA_PATH)
-    ? fs.readFileSync(DATA_PATH, "utf8")
+  const text = fs.existsSync(DATA_DIR)
+    ? fs.readFileSync(DATA_DIR, "utf8")
     : "# Hello mindmap";
   res.send(text);
 }
